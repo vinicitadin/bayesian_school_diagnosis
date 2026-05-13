d494ce6939af4d04c81dee0bd070f2250ac023b6
Vinicius Ferreira Citadin <viniciuscitadin@gmail.com>
2026-05-13 17:06:53 -0300

    Adiciona interface principal WPF para diagnóstico escolar
    Inclui MainWindow.xaml com layout moderno, estilos customizados e formulário para entrada de características do aluno, além de MainWindow.xaml.cs com integração ao DiagnosisViewModel. Permite calcular e exibir o resultado do desempenho escolar via rede bayesiana.

---
+++ BayesianDiagnosis/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using BayesianDiagnosis.ViewModels;
+
+namespace BayesianDiagnosis;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = new DiagnosisViewModel();
+    }
+
+    protected override void OnClosed(EventArgs e)
+    {
+        if (DataContext is DiagnosisViewModel vm)
+        {
+            vm.Dispose();
+        }
+
+        base.OnClosed(e);
+    }
+}
